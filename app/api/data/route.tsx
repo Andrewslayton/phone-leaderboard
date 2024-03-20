dd02b762b382c6b0d39ebe76f6cd124130b6020e
Andrewslayton <andrewsla1726@gmail.com>
2024-03-20 05:07:46 -0400

    MORE VERCEL PROBLEMS

--- app/api/data/route.tsx
+++ app/api/data/route.tsx
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(res: NextApiResponse) {
   const items = await getAllItems();
     return NextResponse.json(items);
 }
