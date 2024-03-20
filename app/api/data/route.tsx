03cb6742a5a348737725a4deda03426d8a585385
Andrewslayton <andrewsla1726@gmail.com>
2024-03-20 18:31:21 -0400

    testing revalidation

--- app/api/data/route.tsx
+++ app/api/data/route.tsx
@@ -3,7 +3,7 @@ import type { NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 
-
+export const revalidate = 60; // 1 minute
 
 export async function GET() {
   const items = await getAllItems();
